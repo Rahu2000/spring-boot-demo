d22b87cda12947626f48e3ebd2cb14613448cb49
rahu2000 <rahu2000@naver.com>
2020-10-20 17:25:39 +0900

    update

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,11 +13,13 @@ public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertTrue("contextLoads", true);
 	}
 
 	@Test
 	public void applicationContextTest() {
 		DemoApplication.main(new String[] {});
+		assertTrue("applicationContextTest", true);
 	}
 
 }
